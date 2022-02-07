6971a045a57192081c46d346e20cbabc155bb0da
Thang. Pham Duc - CMCGlobal DU11 <thangpham.globalcmc@gmail.com>
2022-02-07 17:02:37 +0700

    fixxxx

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
 import { FilesService } from './file.service';
 
+
+class createFileDto {
+  fileBuffer: Buffer
+  fileName: string
+}
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService,
@@ -17,10 +22,18 @@ export class AppController {
   }
 
   @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
 
     console.log("file", file)
-    console.log("fileBuffer", typeof file.buffer)
+    console.log("fileBuffer", file.buffer)
     return this.fileService.uploadFile(file.buffer, file.originalname)
+  }
+
+  @Post('test/upload')
+  async createFile(@Body() createFileDto: createFileDto) {
+    console.log("createFileDto", createFileDto)
+
+    return "201"
   }
 }
